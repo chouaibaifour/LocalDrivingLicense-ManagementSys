6334a33577c692590465cdd35b195eb996aced04
aifourchouaib03@gmail.com <aifourchouaib03@gmail.com>
2024-02-25 12:32:36 +0100

    added frmLogin

--- PresentationLayer/frmLogin.Designer.cs
+++ PresentationLayer/frmLogin.Designer.cs
@@ -28,13 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.pnlLoginInterface = new System.Windows.Forms.Panel();
+            this.pnlLoginForm = new System.Windows.Forms.Panel();
+            this.lbLogo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pnlLoginInterface.SuspendLayout();
+            this.pnlLoginForm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlLoginInterface
+            // 
+            this.pnlLoginInterface.BackColor = System.Drawing.Color.Black;
+            this.pnlLoginInterface.Controls.Add(this.lbLogo);
+            this.pnlLoginInterface.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLoginInterface.Location = new System.Drawing.Point(0, 0);
+            this.pnlLoginInterface.MaximumSize = new System.Drawing.Size(191, 327);
+            this.pnlLoginInterface.MinimumSize = new System.Drawing.Size(191, 327);
+            this.pnlLoginInterface.Name = "pnlLoginInterface";
+            this.pnlLoginInterface.Size = new System.Drawing.Size(191, 327);
+            this.pnlLoginInterface.TabIndex = 1;
+            // 
+            // pnlLoginForm
+            // 
+            this.pnlLoginForm.BackColor = System.Drawing.Color.White;
+            this.pnlLoginForm.Controls.Add(this.linkLabel1);
+            this.pnlLoginForm.Controls.Add(this.textBox2);
+            this.pnlLoginForm.Controls.Add(this.textBox1);
+            this.pnlLoginForm.Controls.Add(this.button1);
+            this.pnlLoginForm.Controls.Add(this.checkBox1);
+            this.pnlLoginForm.Controls.Add(this.label2);
+            this.pnlLoginForm.Controls.Add(this.label3);
+            this.pnlLoginForm.Controls.Add(this.label1);
+            this.pnlLoginForm.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlLoginForm.Location = new System.Drawing.Point(189, 0);
+            this.pnlLoginForm.MaximumSize = new System.Drawing.Size(306, 327);
+            this.pnlLoginForm.MinimumSize = new System.Drawing.Size(306, 327);
+            this.pnlLoginForm.Name = "pnlLoginForm";
+            this.pnlLoginForm.Size = new System.Drawing.Size(306, 327);
+            this.pnlLoginForm.TabIndex = 0;
+            // 
+            // lbLogo
+            // 
+            this.lbLogo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLogo.ForeColor = System.Drawing.Color.White;
+            this.lbLogo.Image = ((System.Drawing.Image)(resources.GetObject("lbLogo.Image")));
+            this.lbLogo.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbLogo.Location = new System.Drawing.Point(0, 33);
+            this.lbLogo.Name = "lbLogo";
+            this.lbLogo.Size = new System.Drawing.Size(188, 230);
+            this.lbLogo.TabIndex = 0;
+            this.lbLogo.Text = "Welcome to  DLVD Mangement System";
+            this.lbLogo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Login to you account";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(99, 167);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 20);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Remember me";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(148, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(99, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(99, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(96, 197);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(95, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Forget Password ?";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(495, 327);
+            this.Controls.Add(this.pnlLoginForm);
+            this.Controls.Add(this.pnlLoginInterface);
+            this.MaximumSize = new System.Drawing.Size(511, 366);
+            this.MinimumSize = new System.Drawing.Size(511, 366);
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            this.pnlLoginInterface.ResumeLayout(false);
+            this.pnlLoginForm.ResumeLayout(false);
+            this.pnlLoginForm.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlLoginInterface;
+        private System.Windows.Forms.Label lbLogo;
+        private System.Windows.Forms.Panel pnlLoginForm;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
