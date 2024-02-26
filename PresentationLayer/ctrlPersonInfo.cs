0db4805265d4f85d6e56e13edf8e269a5b9ed0c6
aifourchouaib03@gmail.com <aifourchouaib03@gmail.com>
2024-02-26 17:15:48 +0100

    Modified ctrlPersonInfo

---
+++ PresentationLayer/ctrlPersonInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLayer;
+namespace PresentationLayer
+{
+    public partial class ctrlPersonInfo : UserControl
+    {
+        public ctrlPersonInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadPersonInfo(int PersonID)
+        {
+            clsPerson person = clsPerson.Find(PersonID);
+
+            if(person == null)
+            {
+                lblPersonID.Text=person.PersonID.ToString();
+                lblNationalNumber.Text = person.NationalNumber; 
+            }
+        }
+
+        private void LoadPersonInfo(string NationalNumber)
+        {
+
+        }
+
+
+    }
+}
