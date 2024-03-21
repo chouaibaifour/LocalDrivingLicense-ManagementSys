6f36b2f4e3dd413a30bf80747d34aec3c57948ed
aifourchouaib03@gmail.com <aifourchouaib03@gmail.com>
2024-03-21 15:58:09 +0100

    #

---
+++ PresentationLayer/Users/ctrlUsersList.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace PresentationLayer.Users
+{
+    partial class ctrlUsersList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRecordCount = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbFilters = new System.Windows.Forms.ComboBox();
+            this.txtFilterValue = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnFindRow = new System.Windows.Forms.Button();
+            this.btnAddNewUser = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AllowUserToAddRows = false;
+            this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.BackgroundColor = System.Drawing.Color.White;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.GridColor = System.Drawing.Color.White;
+            this.dgvUsers.Location = new System.Drawing.Point(3, 44);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.ReadOnly = true;
+            this.dgvUsers.Size = new System.Drawing.Size(854, 262);
+            this.dgvUsers.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "#Records :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRecordCount
+            // 
+            this.lblRecordCount.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordCount.Location = new System.Drawing.Point(102, 328);
+            this.lblRecordCount.Name = "lblRecordCount";
+            this.lblRecordCount.Size = new System.Drawing.Size(63, 24);
+            this.lblRecordCount.TabIndex = 1;
+            this.lblRecordCount.Text = "[???]";
+            this.lblRecordCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 24);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Filter By :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cbFilters
+            // 
+            this.cbFilters.BackColor = System.Drawing.Color.White;
+            this.cbFilters.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbFilters.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbFilters.FormattingEnabled = true;
+            this.cbFilters.Location = new System.Drawing.Point(105, 13);
+            this.cbFilters.Name = "cbFilters";
+            this.cbFilters.Size = new System.Drawing.Size(132, 23);
+            this.cbFilters.TabIndex = 2;
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.BackColor = System.Drawing.Color.White;
+            this.txtFilterValue.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFilterValue.Location = new System.Drawing.Point(255, 13);
+            this.txtFilterValue.Name = "txtFilterValue";
+            this.txtFilterValue.Size = new System.Drawing.Size(155, 22);
+            this.txtFilterValue.TabIndex = 3;
+            this.txtFilterValue.Visible = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-204, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "[???]";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Image = global::PresentationLayer.Properties.Resources.close1;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(765, 328);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(73, 24);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnFindRow
+            // 
+            this.btnFindRow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFindRow.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFindRow.Image = global::PresentationLayer.Properties.Resources.UserFind;
+            this.btnFindRow.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFindRow.Location = new System.Drawing.Point(437, 13);
+            this.btnFindRow.Name = "btnFindRow";
+            this.btnFindRow.Size = new System.Drawing.Size(73, 25);
+            this.btnFindRow.TabIndex = 5;
+            this.btnFindRow.Text = "Find";
+            this.btnFindRow.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnFindRow.UseVisualStyleBackColor = true;
+            this.btnFindRow.Visible = false;
+            this.btnFindRow.Click += new System.EventHandler(this.btnAddNewUser_Click);
+            // 
+            // btnAddNewUser
+            // 
+            this.btnAddNewUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddNewUser.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddNewUser.Image = global::PresentationLayer.Properties.Resources.user_add;
+            this.btnAddNewUser.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAddNewUser.Location = new System.Drawing.Point(765, 13);
+            this.btnAddNewUser.Name = "btnAddNewUser";
+            this.btnAddNewUser.Size = new System.Drawing.Size(73, 25);
+            this.btnAddNewUser.TabIndex = 5;
+            this.btnAddNewUser.Text = "New";
+            this.btnAddNewUser.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAddNewUser.UseVisualStyleBackColor = true;
+            this.btnAddNewUser.Click += new System.EventHandler(this.btnAddNewUser_Click);
+            // 
+            // ctrlUsersList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnFindRow);
+            this.Controls.Add(this.btnAddNewUser);
+            this.Controls.Add(this.txtFilterValue);
+            this.Controls.Add(this.cbFilters);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblRecordCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvUsers);
+            this.Name = "ctrlUsersList";
+            this.Size = new System.Drawing.Size(860, 367);
+            this.Load += new System.EventHandler(this.ctrlUsersList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRecordCount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbFilters;
+        private System.Windows.Forms.TextBox txtFilterValue;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnAddNewUser;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFindRow;
+    }
+}
---
+++ PresentationLayer/Users/ctrlUsersList.cs
@@ -0,0 +1,204 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer.Users
+{
+    public partial class ctrlUsersList : UserControl
+    {
+        public ctrlUsersList()
+        {
+            InitializeComponent();
+        }
+
+        string Filter;
+
+        DataView dataView = clsUser.GetAllUsers().DefaultView;
+
+        private void ctrlUsersList_Load(object sender, EventArgs e)
+        {
+            cbFilters.SelectedIndex = 0;
+            _FillAllUsersTo_dgv();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.FindForm().Close();    
+        }
+
+        private void btnAddNewUser_Click(object sender, EventArgs e)
+        {
+            //frmAddNewUpdateUser addNewUpdateUser = new frmAddNewUpdateUser(-1);
+
+            //addNewUpdateUser.SendDataBack += _FillAllUsersTo_dgv;
+
+            //addNewUpdateUser.ShowDialog();
+
+        }
+
+        private void _FillAllUsersTo_dgv()
+        {
+            DataTable dt = clsUser.GetAllUsers();
+            dgvUsers.DataSource = dt;
+
+            lblRecordCount.Text = dgvUsers.RowCount.ToString();
+
+        }
+
+        private void _getClomnName(int index)
+        {
+            Filter = "";
+
+            string[] Filters = {
+            "UserID",
+            "NationalNumber",
+            "FirstName",
+            "SecondName",
+            "ThirdName",
+            "LastName",
+            "Gender",
+            "Nationality",
+            "Phone",
+            "Email"};
+
+            if (Filters.Length <= index && 0 > index)
+                return;
+
+            Filter = @Filters[index - 1];
+        }
+
+        private void cbFilters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (0 != cbFilters.SelectedIndex)
+            {
+                txtFilterValue.Visible = true;
+
+                btnFindRow.Visible = true;
+
+                return;
+            }
+
+            txtFilterValue.Visible = false;
+
+            btnFindRow.Visible = false;
+
+            dgvUsers.DataSource = dataView;
+
+        }
+
+
+        private void txtFilterValue_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            _getClomnName(cbFilters.SelectedIndex);
+
+
+            if (!"UserID".Equals(Filter))
+            {
+                Filter += @" LIKE '%" + txtFilterValue.Text + "%'";
+            }
+            else
+            {
+                if (!int.TryParse(txtFilterValue.Text, out int UserID))
+                {
+
+                    txtFilterValue.Text = string.Empty;
+                    Filter += @"=0";
+
+                }
+                else
+                {
+
+                    Filter += @"=" + UserID.ToString();
+                }
+
+            }
+
+            try
+            {
+                dataView.RowFilter = Filter;
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            dgvUsers.DataSource = dataView.ToTable();
+
+            lblRecordCount.Text = dgvUsers.RowCount.ToString();
+        }
+
+
+
+        private void smiShowDetails_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(dgvUsers.CurrentRow.Cells[0].Value.ToString(), out int UserID))
+            {
+                //frmUserInfo frmUserInfo = new frmUserInfo(UserID);
+                //frmUserInfo.ShowDialog();
+            }
+
+        }
+
+        private void smiEdit_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(dgvUsers.CurrentRow.Cells[0].Value.ToString(), out int UserID))
+            {
+                //frmAddNewUpdateUser frmUpdateUser = new frmAddNewUpdateUser(UserID);
+                //frmUpdateUser.SendDataBack += _FillAllUsersTo_dgv;
+                //frmUpdateUser.ShowDialog();
+            }
+
+        }
+
+        private void smiDelete_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(dgvUsers.CurrentRow.Cells[0].Value.ToString(), out int UserID))
+            {
+                if (clsUser.IsUserExists(UserID))
+                {
+                    if (clsUser.DeleteUser(UserID))
+                    {
+                        MessageBox.Show("User Deleted Successfully.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("User is Not Deleted.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("User is Not Deleted.");
+                }
+                _FillAllUsersTo_dgv();
+            }
+        }
+
+        private void smiAddNew_Click(object sender, EventArgs e)
+        {
+
+            //frmAddNewUpdateUser frmUpdateUser = new frmAddNewUpdateUser(-1);
+            //frmUpdateUser.SendDataBack += _FillAllUsersTo_dgv;
+            //frmUpdateUser.ShowDialog();
+
+        }
+
+        private void smiCall_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("this Feature is not implemented Yet !");
+        }
+
+        private void smiSendEmail_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("this Feature is not implemented Yet !");
+        }
+    }
+}
