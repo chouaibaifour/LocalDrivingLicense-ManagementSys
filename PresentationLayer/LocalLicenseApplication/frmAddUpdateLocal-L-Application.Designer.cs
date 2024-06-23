1cae570f5d9a8f60c8f6d66242855ece6635bb4b
aifourchouaib03@gmail.com <aifourchouaib03@gmail.com>
2024-06-23 07:13:47 +0100

    #

---
+++ PresentationLayer/LocalLicenseApplication/frmAddUpdateLocal-L-Application.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace PresentationLayer.LocalLicenseApplication
+{
+    partial class frmAddUpdateLocal_L_Application
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmAddUpdateLocal_L_Application
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "frmAddUpdateLocal_L_Application";
+            this.Text = "frmAddUpdateLocal_L_Application";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ PresentationLayer/LocalLicenseApplication/frmAddUpdateLocal-L-Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer.LocalLicenseApplication
+{
+    public partial class frmAddUpdateLocal_L_Application : Form
+    {
+        public frmAddUpdateLocal_L_Application()
+        {
+            InitializeComponent();
+        }
+    }
+}
