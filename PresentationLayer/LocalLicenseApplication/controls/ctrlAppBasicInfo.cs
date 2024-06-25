4ecb8406e9cef462d532b82c8ba162bc81a6aa98
aifourchouaib03@gmail.com <aifourchouaib03@gmail.com>
2024-06-25 18:48:48 +0100

    #

---
+++ PresentationLayer/LocalLicenseApplication/controls/ctrlAppBasicInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer.LocalLicenseApplication.controls
+{
+    public partial class ctrlAppBasicInfo : UserControl
+    {
+        public ctrlAppBasicInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void LoadApplicationBasicInfo(int ApplicationID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
